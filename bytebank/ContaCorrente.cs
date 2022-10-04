38f1b5c8b669f4b83c831297ecbd5cb6fd6b9025
André Bessa <andrebessax@gmail.com>
2022-10-04 17:54:02 -0300

    Update: Ajustes.

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -10,7 +10,41 @@ namespace bytebank
     {
         public int numero_agencia;
         public string conta;
+        public double saldo = 100;
+
         public string titular;
-        public double saldo;
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
     }
 }
